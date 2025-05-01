0e9336019dc1b3ec75e0c049c49bf293ba7e7508
wsfdl <vanderliang@gmail.com>
2025-05-01 15:07:21 +0800

    add data api for cn stock index

---
+++ irolling/data/constants.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2025 irolling
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""constants for data"""
+
+SSE50_SYMBOL = "000016"
+CSI300_SYMBOL = "000300"
+CSI500_SYMBOL = "000905"
+CSI1000_SYMBOL = "000852"
