d26b2556a7d4a46b70435b2e6f0f8506bc983fe9
wsfdl <vanderliang@gmail.com>
2025-05-12 11:06:47 +0800

    refine get_contract_basis according to trading time

--- irolling/entry.py
+++ irolling/entry.py
@@ -25,29 +25,21 @@ from irolling.data import api
 from irolling.data import constants
 
 
-def get_contract_basis():
-    """show current basis"""
-
-    # check the trading time
-    now = datetime.datetime.now()
-    trading_date = calendar.last_trading_day(now.date())
-    if calendar.is_trading_time(now):
-        # NOTE(fixme, support the real time and today)
-        raise NotImplementedError
-
+def get_contract_basis_by_date(date):
+    """get contract basis by date"""
     # symbol_expire_map
     symbol_expire_map = api.get_symbol_expire_map(
-        trading_date.strftime("%Y%m%d"),
+        date.strftime("%Y%m%d"),
     )
 
     # get future price
-    futures = api.get_stock_index_futures_daily(trading_date, trading_date)
+    futures = api.get_stock_index_futures_daily(date, date)
 
     basis_list = []
     for variety, index_symbol in constants.VARIETY_SYMBOL_MAP.items():
         # get spot price
         spot = api.get_stock_index_daily(symbol=index_symbol)
-        spot_price = spot.loc[trading_date]["close"]
+        spot_price = spot.loc[date]["close"]
 
         # filter the contracts by variety
         contracts = futures[futures["variety"].isin([variety])]
@@ -62,7 +54,7 @@ def get_contract_basis():
             future_price = row["close"]
             symbol = row["symbol"]
             expire = symbol_expire_map[symbol]
-            days = calendar.delta_days(trading_date, expire)
+            days = calendar.delta_days(date, expire)
 
             # initiate the basis class for different symbol
             basis_list.append(
@@ -70,6 +62,39 @@ def get_contract_basis():
             )
 
     return basis_list
+
+
+def get_contract_basis():
+    """show current basis"""
+
+    # check the trading time
+    now = datetime.datetime.now()
+    today = now.date()
+
+    if calendar.is_trading_day(today):
+        # if today is a trading day and the market has not open, use
+        # the last trading day data
+        if calendar.is_before_open(now):
+            last_trading_date = calendar.last_trading_day(today)
+            return get_contract_basis_by_date(last_trading_date)
+
+        # if today is a trading day and the market has closed, use
+        # today's data
+        if calendar.is_after_close(now):
+            return get_contract_basis_by_date(today)
+
+        # if today is a trading day and is on trading time, use the realtime
+        # data
+        if calendar.is_trading_time(now):
+            # NOTE(fixme, support the real time and today)
+            raise NotImplementedError
+
+        # if today is a trading day and is in the breaking time for noon
+        raise NotImplementedError
+
+    # if today is not a trading date, use the latest trading day
+    last_trading_date = calendar.last_trading_day(today)
+    return get_contract_basis_by_date(last_trading_date)
 
 
 def do_list_basis(_):
